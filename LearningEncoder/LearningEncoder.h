da977b0a5f37e78d4578f59a9063ee3c505a8405
Damiaan Dufaux <damiaan@dpro.be>
2018-01-27 18:50:32 +0100

    Initial Commit

---
+++ LearningEncoder/LearningEncoder.h
@@ -0,0 +1,19 @@
+//
+//  LearningEncoder.h
+//  LearningEncoder
+//
+//  Created by Damiaan on 27/01/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LearningEncoder.
+FOUNDATION_EXPORT double LearningEncoderVersionNumber;
+
+//! Project version string for LearningEncoder.
+FOUNDATION_EXPORT const unsigned char LearningEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LearningEncoder/PublicHeader.h>
+
+
